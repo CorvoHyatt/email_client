22331d10f1f1520ba58f5d5ff86a15909d1fbd62
CorvoHyatt <corvohyatt@gmail.com>
2023-02-04 23:41:14 -0600

    server deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-
+import { Email } from './models/email.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -14,20 +14,26 @@ export class AppComponent implements OnInit {
     backgroundColor: '#ffffff'
   }
 
-  mensaje:string = "Hola"
+  mensaje: string = "Hola"
 
 
   config = {
     toolbar: [
-      ['bold','italic','underline'],
+      ['bold', 'italic', 'underline'],
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-      [{ 'color': ['#000000','#229954','#FF5733','#2E86C1','#DC7633','#00FF00','#BDB76B'] }, { 'background': [] }],
+      [{ 'color': ['#000000', '#229954', '#FF5733', '#2E86C1', '#DC7633', '#00FF00', '#BDB76B'] }, { 'background': [] }],
     ]
   }
 
   onSubmit() {
     this.mensaje = this.editorForm.get('editor')!.value
     console.log(this.editorForm.get('editor')!.value)
+    let email = new Email()
+    email.contenido = this.editorForm.get('editor')!.value
+    email.enviado_por = "corvohyatt@gmail.com"
+    email.recibido_por = "snifex@gmail.com"
+    email.asunto = "Saludos cordiales"
+    email.fecha = "10/10/10"
   }
 
   ngOnInit() {
--- src/app/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import { Email } from "../models/email.model";
-var fs = require("fs");
-export function save_email(email: Email) {
-
-  let file_name = email.asunto + email.enviado_por;
-  let data = JSON.stringify(email);
-  fs.writeFile(file_name + ".json", data, ((err: any) => { console.log(err) }))
-
-}

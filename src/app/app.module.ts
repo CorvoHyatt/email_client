f4adfb5db4d609033e3f4553f344d9271962bd26
CorvoHyatt <corvohyatt@gmail.com>
2023-02-06 12:00:55 -0600

    Avance Services, servidor dir deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuillModule } from 'ngx-quill'
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -13,6 +14,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HttpClientModule,
     QuillModule.forRoot()
   ],
   providers: [],
--- src/server/tools.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def create_dir(path: string):
-    os.system('mkdir {path{send recive')
-
-
-if __name__ == "__main__":
-path = "."
